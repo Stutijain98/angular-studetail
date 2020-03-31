e2d2541c50214fc9801f321f0bbd8a3bfe45436d
Stutijain98 <60315630+Stutijain98@users.noreply.github.com>
2020-03-31 20:02:32 +0530

    Add files via upload

---
+++ StudentDetail.cs
@@ -0,0 +1,10 @@
+public class StudentDetail
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public int MobileNo. { get; set;}
+    public string Email {get; set;}
+    public string Course {get; set;}
+
+   
+}
